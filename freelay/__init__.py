0ba7f9352dbd285eafc07ab85ac6d16ee91a0b5e
Francois <francois.sharpe@live.com>
2021-04-13 18:54:19 +0200

    forwarding logs to stdout and stderr

--- freelay/__init__.py
+++ freelay/__init__.py
@@ -1,9 +1,20 @@
+import logging
+import sys
 from typing import Optional
 
 from fastapi import FastAPI
 
 from .api import router as api_router
 from .db import Base, engine
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
 
 Base.metadata.create_all(bind=engine)
 
