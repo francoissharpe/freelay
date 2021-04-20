239f53aec72e7ea8deef66d555523cfa12865d11
Francois <francois.sharpe@live.com>
2021-04-20 22:47:55 +0200

    added incoming email endpoint

--- freelay/api/v1/__init__.py
+++ freelay/api/v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from .emails.endpoints import router as email_router
 from .users.endpoints import router as user_router
 
 
 router = APIRouter(prefix="/v1")
 router.include_router(user_router)
+router.include_router(email_router)
